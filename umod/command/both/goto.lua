742db38f70f1af0824a1444d1736dd29593bfdf5
cyrillePrigent <cyrille.prigent@laposte.net>
2017-11-27 20:35:50 +0100

    - Spam reduction for user command  - Clean iwant command file  - Clean goto command file  - Check all parameters for iwant & goto command

--- umod/command/both/goto.lua
+++ umod/command/both/goto.lua
@@ -1,22 +1,35 @@
 -- Move directly your player to another player.
+-- From kmod.lua
 --  params is parameters passed from et_ClientCommand / et_ConsoleCommand function.
 --   * params["arg1"] => player ID
 --   * params["arg2"] => target
 function execute_command(params)
+    params.say = msgCmd["chatArea"]
+
     if params.nbArg < 3 then
         printCmdMsg(params, "Useage: goto \[name/PID\] \[name/PID - Destination\]\n")
     else
-        targetNum = client2id(params["arg2"], params)
+        clientNum = client2id(params["arg1"], params)
 
-        if client[targetNum]['team'] >= 3 or client[targetNum]['team'] < 1 then
-            printCmdMsg(params, "Client is not actively playing\n")
+        if client[clientNum]['team'] ~= 1 and client[clientNum]['team'] ~= 2 then
+            printCmdMsg(params, "Client (to move) is not actively playing\n")
         else
-            if et.gentity_get(targetNum, "health") <= 0 then
-                printCmdMsg(params, "Client is currently dead\n")
+            if et.gentity_get(clientNum, "health") <= 0 then
+                printCmdMsg(params, "Client (to move) is currently dead\n")
             else
-                local targetOrigin = et.gentity_get(targetNum, "origin")
-                targetOrigin[2] = targetOrigin[2] + 40
-                et.gentity_set(params["arg1"], "origin", targetOrigin)
+                targetNum = client2id(params["arg2"], params)
+
+                if client[targetNum]['team'] ~= 1 and client[targetNum]['team'] ~= 2 then
+                    printCmdMsg(params, "Client (Destination) is not actively playing\n")
+                else
+                    if et.gentity_get(targetNum, "health") <= 0 then
+                        printCmdMsg(params, "Client (Destination) is currently dead\n")
+                    else
+                        local targetOrigin = et.gentity_get(targetNum, "origin")
+                        targetOrigin[2] = targetOrigin[2] + 40
+                        et.gentity_set(clientNum, "origin", targetOrigin)
+                    end
+                end
             end
         end
     end
